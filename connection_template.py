9b7aa9c300bc611641a6fb2a8c6bad844ecd3371
Vaibhav Goel <vaibhavg133@github.com>
2019-09-17 07:02:15 +0530

    Minor Changes

--- connection_template.py
+++ connection_template.py
@@ -6,10 +6,10 @@ import math
 import argparse
 
 def connectMyCopter():
-	parser = argparse.ArguementParser(description="commands")
-	parser.add_arguement('--connect')
+	parser = argparse.ArgumentParser(description="commands")
+	parser.add_argument('--connect')
 	args = parser.parse_args()
-	connection_string = arg.connect
+	connection_string = args.connect
 	vehicle=connect(connection_string,wait_ready=True)
 	return vehicle
 # python connection_template.py --connect 127.0.0.1:14550
